29537dda9f8681d14694f0120d89662003f3694c
maxim satanov <satanov201354@gmail.com>
2026-01-13 14:12:15 +0500

    refactor: remove domain layer and repositories

--- composeApp/src/jvmMain/kotlin/org/example/project_dw/domain/repositories/IStatisticsRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.example.project_dw.domain.repositories
-
-import org.example.project_dw.domain.models.StationarityResult
-
-interface IStatisticsRepository {
-    suspend fun checkStationarity(data: DoubleArray): StationarityResult
-}
--- composeApp/src/jvmMain/kotlin/org/example/project_dw/shared/datasources/python/PythonCommands.kt
+++
@@ -1,8 +0,0 @@
-package org.example.project_dw.shared.datasources.python
-
-object PythonCommands {
-    const val ADF_TEST = "adf_test"
-    const val KPSS_TEST = "kpss_test"
-    const val ENGLE_GRANGER = "engle_granger"
-    const val ECM_MODEL = "ecm_model"
-}
